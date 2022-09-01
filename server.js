81d81521bf4ebb73e722d3d7b03542361a9a184d
Yao Jiang <yaojiang7595@gmail.com>
2022-09-01 18:37:02 -0400

    Register route posting on DB

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.once('open', () => {
   console.log('connected to mongo');
 });
 
-app.get('/login');
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+  User.create(req.body, () => {
+    res.send('created!');
+  });
+});
 
 app.listen(port, () => {
   console.log('I am listening on port', port);
