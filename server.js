75787c5a60f5a706b1afdd392aebf879cb811f2c
Yao Jiang <yaojiang7595@gmail.com>
2022-09-02 10:13:13 -0400

    Added hashing and jwt token to register and login

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const secret = process.env.SECRET;
 
 const mongoose = require('mongoose');
 const User = require('./models/user');
@@ -16,9 +19,38 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/protected', (req, res) => {
+  res.send('This should be protected!');
+});
+
 app.post('/register', (req, res) => {
-  User.create(req.body, () => {
-    res.send('created!');
+  const { password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 12);
+  User.create({ ...req.body, password: hashedPassword }, (err, newUser) => {
+    const { email, firstName, lastName } = newUser;
+    const id = newUser._id.toString();
+    const token = jwt.sign({ id, email, firstName, lastName }, secret, {
+      expiresIn: '8h',
+    });
+    res.json({ id, firstName, lastName, token });
+  });
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      res.json({ error: 'Invalid Credentials' });
+    } else {
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        res.json({ error: 'Invalid Credentials' });
+      } else {
+        const { id, email, firstName, lastName } = user;
+        const token = jwt.sign({ id, email, firstName, lastName }, secret, {
+          expiresIn: '8h',
+        });
+        res.json({ id, email, firstName, lastName, token });
+      }
+    }
   });
 });
 
