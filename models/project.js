2aeede2f20ea59639d124439280c0d435074be1f
Yao Jiang <yaojiang7595@gmail.com>
2022-09-10 15:06:06 -0400

    task CRUD fully implemented

--- models/project.js
+++ models/project.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Task = require('./task');
 
 const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -6,6 +7,16 @@ const projectSchema = new mongoose.Schema({
   creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+});
+
+projectSchema.post('findOneAndRemove', async function (project) {
+  if (project) {
+    await Task.deleteMany({
+      _id: {
+        $in: project.tasks,
+      },
+    });
+  }
 });
 
 const Project = mongoose.model('Project', projectSchema);
